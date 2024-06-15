33b34e92ef94ba03103bb65988bb2f73f0b874f6
Tsanglung <ab1726531@gmail.com>
2024-06-15 17:39:33 +0800

    Data Structure Chap 1

---
+++ DataStructure/Basic Concepts/Fibonacci.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int Fibonacci (int n)// ¶O¤ó¼Æ¦C
+{
+   if(n == 0) return 0;
+   else if(n == 1) return 1;
+   else return Fibonacci(n - 1) + Fibonacci(n - 2);
+} 
+
+int main()
+{
+  int n;
+  cout << "Input n : ";
+  cin >> n;
+  cout << "Fibonacci("  << n << ") is " << Fibonacci(n) << endl;
+}
