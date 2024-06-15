4d275d91c1c21d2cf16f6b95ef1f3b7e60e94251
Tsanglung <ab1726531@gmail.com>
2024-06-15 18:58:34 +0800

    Data Structures Chap 1

---
+++ Data Structure/Basic Concepts/BinomialCoefficient.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//二項式係數
+int Binomial(int n,int m)
+{
+    if (m == 0 || m == n) return 1;
+    else return Binomial(n - 1, m) + Binomial(n - 1, m - 1);
+}
+
+int main()
+{
+    int m , n;
+    cout << "Input m , n : ";
+    cin >> m >> n;
+    cout<< "Binomial Coefficient(" << m << "," << n <<") = " << Binomial(m , n) << endl; 
+}
---
+++ Data Structure/Basic Concepts/Factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int RFactorial(int n)//»¼°j¶¥­¼
+{
+    if(n <= 1) return 1;
+    else return n * RFactorial(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n << "! = " << RFactorial(n) << endl;
+}
