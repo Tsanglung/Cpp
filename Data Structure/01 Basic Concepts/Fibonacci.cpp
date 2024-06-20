4a27ad2d6d2b230d5a8c4e5bff5a60607f70ee7b
Tsanglung <ab1726531@gmail.com>
2024-06-20 14:33:52 +0800

    Fibonacci

--- Data Structure/01 Basic Concepts/Fibonacci.cpp
+++ Data Structure/01 Basic Concepts/Fibonacci.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 using namespace std;
-int Fibonacci (int n)// 費氏數列
+int RFibonacci (int n)// 費氏數列
 { 
    if(n == 0) return 0; 
    else if(n == 1) return 1;
-   else return Fibonacci(n - 1) + Fibonacci(n - 2);
+   else return RFibonacci(n - 1) + RFibonacci(n - 2);
 } 
+
+void Fibonacci(int n)
+{
+  if( n <= 1) cout << n << endl;
+  else {
+    int fn,fn1 = 1,fn2 = 0;
+    for(int i = 2; i <= n ; i++){
+      fn = fn1 + fn2;
+      fn2 = fn1;
+      fn1 = fn;
+    }
+    cout << "Fibonacci("  << n << ") is " << fn << endl;
+  }
+}
 
 int main() 
 {
   int n;
   cout << "Input n : ";
   cin >> n;
-  cout << "Fibonacci("  << n << ") is " << Fibonacci(n) << endl;
+  cout << "Fibonacci("  << n << ") is " << RFibonacci(n) << endl;
+  Fibonacci(n);
 }
