e4fecce78bd2a4bf2f75cc521325339aefd9203e
Tsanglung <ab1726531@gmail.com>
2024-06-28 23:35:07 +0800

    ADT 3.1,Program 4.19、4.20 template class linked stacks

---
+++ Data Structure/04 Linked List/Linked Stack and Queue/Linked Stack/LinkedStack.cpp
@@ -0,0 +1,33 @@
+#include"LinkedStack.h"
+
+template<class T>
+LinkedStack<T>::LinkedStack(){ top = NULL;}
+
+template<class T>
+inline bool LinkedStack<T>::IsEmpty() const{ return top == NULL;}
+
+template<class T>
+inline T&LinkedStack<T>::Top()const{ return top->data; }
+
+template<class T>
+void LinkedStack<T>::Push(const T& e){top = new StackNode<T>(e,top);}
+
+template<class T>
+void LinkedStack<T>::Pop()
+{
+    StackNode<T> *delNode = top;
+    top = top->link;
+    delete delNode;
+}
+ 
+template<class T>
+ostream& operator<<(ostream& os,const LinkedStack<T>& s)
+{
+    StackNode<T>* currentNode = s.top;
+    while(currentNode != NULL)
+    {
+        os<<currentNode->data<<" ";
+        currentNode = currentNode->link;
+    }
+    return os;
+}
