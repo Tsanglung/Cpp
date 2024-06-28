7bafa3a56a35f560ea938c246a013956f1d7ff68
Tsanglung <ab1726531@gmail.com>
2024-06-28 22:49:54 +0800

    Exercise 10 recursive ackermann;s function

--- Data Structure/01 Basic Concepts/Ackermann.cpp
+++ Data Structure/01 Basic Concepts/Ackermann.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //ªü§J°Ò¨ç¼Æ
 int Ackermann(int m,int n)
-{
+{   //recursive
     if(m == 0) return n + 1; 
     else if(n == 0) return Ackermann(m - 1, 1);
     else return Ackermann(m - 1, Ackermann(m, n - 1));
