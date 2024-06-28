f81d47c6a5cc62a70981501041b675d4ceac2203
Tsanglung <ab1726531@gmail.com>
2024-06-28 22:45:53 +0800

    Exercise 6 recursive and iterative factorial function

--- Data Structure/01 Basic Concepts/Factorial.cpp
+++ Data Structure/01 Basic Concepts/Factorial.cpp
@@ -7,9 +7,17 @@ int RFactorial(int n)//»¼°j¶¥­¼
     else return n * RFactorial(n - 1);
 } 
 
+int Factorial(int n)
+{
+    int F = 1;
+    for(int i = n;i > 0;i--) F *= i;
+    return F;
+}
+
 int main()
 {
     int n;
     cin >> n;
     cout << n << "! = " << RFactorial(n) << endl;
+    cout << n << "! = " << Factorial(n) << endl;
 }
